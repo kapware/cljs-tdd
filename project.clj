72dd70222b052907644bf276c1e695cf175219a3
Pawel Kapala <github@kapware.com>
2017-04-15 22:31:34 +0200

    Repository url updated

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject cljs-tdd/lein-template "0.0.1"
-  :description "Created with lein-create-template"
-  :url "http://example.com/FIXME"
+  :description "Testing ready cljs lein template"
+  :url "https://github.com/kapware/cljs-tdd"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
